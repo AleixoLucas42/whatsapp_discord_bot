7b4bd002061358b31026a1cb64494a69e606beb0
AleixoLucas42 <aleixo2lucas@gmail.com>
2024-04-10 19:57:31 -0300

    Adicionado aviso quando o bot estiver online

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import time
 wpp_url = "http://whatsapp:3000/client/sendMessage/DISC_WPP_SESSION"
 chat_id = os.environ["CHAT_ID"]
 token = os.environ["DISC_TOKEN"]
-
+bot_status_working = False
 last_notification_time = {}
 
 def send_wpp_notification(content):
@@ -21,12 +21,12 @@ def send_wpp_notification(content):
         'Content-Type': 'application/json'
     }
 
-    response = requests.request("POST", wpp_url, headers=headers, data=payload)
+    response = requests.request("POST", wpp_url, headers=headers, data=payload, verify=False)
 
     if response.status_code == 200:
-        print("> whatsapp message sent")
+        print("[+] whatsapp message sent")
     else:
-        print(f"Error {response.status_code} sending message\n")
+        print(f"[!] error {response.status_code} sending message\n")
         print(response.text)
 
 intents = discord.Intents.all()
@@ -36,7 +36,14 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot conectado como {bot.user.name}')
+    global bot_status_working
+    print(f'[*] bot connected as {bot.user.name}')
+    while (bot_status_working is not True):
+        time.sleep(30)
+        print("[*] trying to wake up bot")
+        if (send_wpp_notification("✅ bot is awake")):
+            bot_status_working = True
+    print("[*] bot waked up")
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -44,7 +51,7 @@ async def on_voice_state_update(member, before, after):
         if after.channel is not None:
             channel_name = after.channel.name
             guild_name = after.channel.guild.name
-            print(f"> {member.name} connected to voice channel {channel_name}")
+            print(f"[+] {member.name} connected to voice channel {channel_name}")
 
             if member.id not in last_notification_time or time.time() - last_notification_time[member.id] > 30:
                 send_wpp_notification(f"{member.name} on: {channel_name}")
